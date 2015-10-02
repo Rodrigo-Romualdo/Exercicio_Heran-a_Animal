1b1ae6a8a995e516bb8a0711a966b5e8f98beb7e
Rodrigo-Romualdo <rodrigo.s.romualdo@ucl.br>
2015-10-02 18:15:46 -0300

    Teste

---
+++ TesteWord/TesteWord/Word.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+
+namespace TesteWord
+{
+    class Word
+    {
+        Dim word As Application = New Application()
+        Dim doc As Document = word.Documents.Open("c:\document.docx")
+        doc.Activate()
+        doc.SaveAs2("c:\document.pdf", WdSaveFormat.wdFormatPDF)
+        doc.Close()
+
+
+
+    }
+}
